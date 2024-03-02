e65c4eb3b69facecdc117ac501917acd682b569d
axel-paillaud <contact@axelweb.fr>
2024-03-02 14:04:46 +0100

    init signals functions _on_score_timer_timeout and _on_start_timer_timeout

--- main.gd
+++ main.gd
@@ -12,3 +12,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$ScoreTimer.start()
+
+func _on_score_timer_timeout():
+	score += 1
+	
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
